21cc156a8cf669844688927ad4ce9da5b05a3df6
luanaflorencio <lua.m.016@gmail.com>
2021-03-30 18:22:18 -0300

    migrations e os atributos das entidades

---
+++ database/migrations/2021_03_30_184415_create_disciplinas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDisciplinasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('disciplinas', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo');
+            $table->string('nome');
+            $table->string('professor');
+            $table->integer('estudantes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('disciplinas');
+    }
+}
---
+++ database/migrations/2021_03_30_185801_create_professors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfessorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('professors', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo');
+            $table->string('nome');
+            $table->string('cpf');
+            $table->date('nascimento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('professors');
+    }
+}
