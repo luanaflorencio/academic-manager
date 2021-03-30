c8ab5ef3ff105ab55d2fb24d43a4972a5d42421b
luanaflorencio <lua.m.016@gmail.com>
2021-03-30 18:25:03 -0300

    Criando Factories e seeders

---
+++ database/factories/EstudanteFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Estudante;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EstudanteFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Estudante::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
